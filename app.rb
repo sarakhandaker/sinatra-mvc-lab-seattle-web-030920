74838a118f4fccfe7db93cfac7f0e27e3eb679f0
github username <github email address>
2020-03-31 10:03:32 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get "/" do 
+        erb :user_input
+    end
+
+    post "/piglatinize" do 
+    #receive your form's params.
+    @piglatin=PigLatinizer.new.piglatinize(params[:user_phrase])
+        erb :piglatin
+
+    end
+
 end
